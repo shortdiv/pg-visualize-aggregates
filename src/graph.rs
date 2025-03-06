ce2ab5c451e7b1cb31a65d63ccf8e2bd0e1e9202
Divya <hello@shortdiv.com>
2025-03-06 17:46:38 -0500

    feat: draw a basic svg by pulling in a a template and updating attrs via minijinja

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,4 @@
+use minijinja::{context, Environment};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug)]
@@ -21,50 +22,16 @@ impl Graph {
             colour,
         }
     }
-    pub fn draw_svg(&self) -> CodepenJson {
-        // Load templates from the "templates" folder using a glob pattern
-        // let mut context = Context::new();
-        // //hardset the padding around the graph
-        // let padding = 50;
-        //
-        // //ensure the viewbox is as per input
-        // let width = width - padding * 2;
-        // let height = height - padding * 2;
-        //
-        // context.insert("name", &self.name);
-        // context.insert("width", &width);
-        // context.insert("height", &height);
-        // context.insert("padding", &padding);
-        //
-        // let str = r#"
-        // <?xml version="1.0" standalone="no"?> \
-        // <svg \
-        //   width="100%" \
-        //   height="100%" \
-        //   viewBox="0 0 {{height + padding * 2}} {{width + padding * 2}}" \
-        //   preserveAspectRatio="xMidYMid meet" \
-        //   xmlns="http://www.w3.org/2000/svg" \
-        //   > \
-        //   <text \
-        //     x="{{width/2 + padding}}" \
-        //     y="{{padding / 2}}" \
-        //     font-family="-apple-system, system-ui, BlinkMacSystemFont, Roboto" \
-        //     dominant-baseline="middle" \
-        //     text-anchor="middle" \
-        //     font-size="18" \
-        //     fill="red" \
-        //     font-weight="700" \
-        //     > \
-        //     {{name}} \
-        //   </text> \
-        // </svg>"#;
-        // let rendered = Tera::one_off(include_str!("templates/svg_template.html"), &context, true).expect("Could not draw graph");
-        //let rendered = tera.render("svg_template.html", &context).unwrap();
+    pub fn draw_svg(&self, template: &str) -> CodepenJson {
+        let mut env = Environment::new();
+        env.add_template("svg", template).unwrap();
+        let tmpl = env.get_template("svg").unwrap();
+        let rendered = tmpl.render(context!(width => 40, height => 40)).unwrap();
+
         let json = CodepenJson {
-            title: "New Pen".to_string(),
-            html: "<div>Hello, World!</div>".to_string(),
+            title: "This is a Pen".to_string(),
+            html: rendered,
         };
-        println!("{}", "iummm hello???");
         return json;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,15 @@ mod seed;
 use pgrx::prelude::*;
 
 use anyhow::Result;
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, RequestBuilder};
 use serde_json;
+use std::collections::HashMap;
 
 use crate::graph::Graph;
-use crate::seed::load_climb_data;
 
 ::pgrx::pg_module_magic!();
+
+const SVG: &str = include_str!("templates/svg_template.html");
 
 #[pg_extern]
 fn hello_visualizing_aggregates() -> &'static str {
@@ -21,62 +23,30 @@ fn hello_visualizing_aggregates() -> &'static str {
 #[pg_extern]
 fn draw_graph() -> Result<String> {
     let graph = Graph::new("Example".to_string(), 40, "#8ff0a4".into());
-    let svg = graph.draw_svg();
-    let svg_title = serde_json::to_string(&svg.title).unwrap();
-    let svg_html = serde_json::to_string(&svg.html).unwrap();
+    let svg = graph.draw_svg(SVG);
+    let mut form = HashMap::new();
+    form.insert("title", svg.title);
+    form.insert("html", svg.html);
 
-    let dat: String = format!(r#"{{"title": {}, "html": {}}}"#, svg_title, svg_html);
-    let form_data = format!("data={}", dat);
+    let dat1 = serde_json::to_string(&form).unwrap();
+
+    pgrx::info!("{}", dat1);
+
+    // let dat: String = format!(r#"{{"title": {}, "html": {}}}"#, svg_title, svg_html);
+    let form_data = format!("data={}", dat1);
+    // pgrx::info!("{}", svg);
 
     let client = Client::new();
+
     let res = client
         .post("https://codepen.io/pen/define")
         .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(form_data.clone())
+        .body(form_data)
         .send()?;
 
+    // pgrx::info!("{:?}", res.body());
+
     Ok(res.url().to_string())
-}
-
-#[pg_extern]
-fn insert_climb_data() -> String {
-    let climbs = load_climb_data();
-    let keys = [
-        "id",
-        "route_name",
-        "parent_sector",
-        "route_id",
-        "sector_id",
-        "type_string",
-        "fa",
-        "yds",
-        "vermin",
-        "nopm_yds",
-        "nopm_vermin",
-        "yds_rank",
-        "vermin_rank",
-        "safety",
-        "parent_loc",
-        "description",
-        "location",
-        "protection",
-        "corrected_users_ratings",
-    ];
-    let mut climb_values = vec![];
-
-    for climb in &climbs {
-        let mut vals: Vec<String> = vec![];
-        for k in &keys {
-            let serialized = serde_json::to_value(&climb).unwrap();
-            vals.push(serialized[k].to_string());
-        }
-        climb_values.push(format!("({})", vals.join(",")));
-    }
-    return format!(
-        "INSERT INTO climbs({}) VALUES {}",
-        keys.join(","),
-        climb_values.join(",")
-    );
 }
 
 #[cfg(any(test, feature = "pg_test"))]
@@ -92,17 +62,16 @@ mod tests {
         );
     }
 
-    #[pg_test]
-    fn test_insert() {
-        // tbh not sure how to run this in the extension
-        assert!(crate::insert_climb_data().contains("INSERT"));
-        assert!(crate::insert_climb_data().contains("VALUES"));
-    }
-
-    #[pg_test]
-    fn test_iterator() {
-        assert_eq!(1, crate::to_day("1 Jul 2003 10:52:37 +0200"))
-    }
+    // #[pg_test]
+    // // fn test_insert() {
+    // //     // tbh not sure how to run this in the extension
+    // //     assert!(crate::insert_climb_data().contains("INSERT"));
+    // //     assert!(crate::insert_climb_data().contains("VALUES"));
+    // // }
+    // #[pg_test]
+    // fn test_iterator() {
+    //     assert_eq!(1, crate::to_day("1 Jul 2003 10:52:37 +0200"))
+    // }
 }
 
 /// This module is required by `cargo pgrx test` invocations.
